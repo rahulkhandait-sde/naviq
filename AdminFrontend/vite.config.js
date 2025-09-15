647d219886e536d7b36f21845f2d3ebedddd778f
Gourab Das <144489254+Gourab2005@users.noreply.github.com>
2025-09-15 16:47:26 +0530

    Update vite.config.js

--- AdminFrontend/vite.config.js
+++ AdminFrontend/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/naviq/', 
-  server: {
-    proxy: {
-      "/rurl": {
-        target: "http://localhost:5000", // your backend URL
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/naviq/', 
+//   server: {
+//     proxy: {
+//       "/rurl": {
+//         target: "http://localhost:5000", // your backend URL
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// })
